d931e8a771380196ff1d1f3106f6bb8acb753b2f
Rafael Jesus <rafaelljesus86@gmail.com>
2016-06-12 19:11:15 -0300

    Setup circleci

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ElbRegistrator.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -28,6 +30,21 @@ defmodule ElbRegistrator.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:exrm, "~> 1.0.5"}
+    ]
+  end
+
+  defp description do
+    """
+    Register EC2 instances with an ELB
+    """
+  end
+
+  defp package do
+    [maintainers: ["Rafael Jesus"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/rafaeljesus/elb_registrator"},
+     files: ~w(mix.exs README.md lib)]
   end
 end
